5def0b7e495fb5035239ecceb293d7915292e26f
Geraldo Kranenburg <geraldo.kranenburg@outlook.com>
2024-06-09 19:09:12 +0800

    created a valid image component to check the images

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import { Button, Stack, Typography, Box } from '@mui/material'
+import { checkImageURL } from '../utils/ValidImage'
 
 const JobCard = (job) => {
   console.log(job)
+  const validImageURL = checkImageURL(job.job.employer_logo)
+  ? job.job.employer_logo
+  : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg";
+
   return (
     <Box className="container">
       <Link className="logo-container" to={`/job/${job.id}`} >
-      <img className="logo-image" src={job.job.employer_logo} alt={job.job.employer_name} loading="lazy" />
+      <img className="logo-image" src={validImageURL} alt={job.job.employer_name} loading="lazy" />
       </Link>
+      
       <Typography className="company-name">
         {job.job.employer_name}
       </Typography>
